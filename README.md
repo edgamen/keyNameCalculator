3b8deb0fff92e4ba4ad60c46ece7017796787dd4
Edgardo <edgamencontacto@gmail.com>
2019-01-12 22:38:58 -0500

    Inicio proyecto
    Copia de carpetas con html y css del proyecto y primera parte de codificación del archivo main.js. Correccion de algunos estilos.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 keyNameCalculator
+
+La idea es seguir las instrucciones del training  e ir apropiando los conceptos de manipulacion del DOM por medio de Javascript.
---
+++ starter_project_calculator/js/main.js
@@ -0,0 +1,97 @@
+// calculator
+
+//Global vars
+
+var num1="";
+var num2="";
+var operator;
+var flag=0;
+var display= document.getElementById('display');
+var equalTo=0;
+
+//funcion setNumber(valor)
+
+function setValue(number){
+    if(equalTo===1){
+        clearButton();
+    }
+    if(flag===0){
+        num1+=number;
+        //alert(num1)
+        display.innerHTML+=number;
+    }else{
+        num2+=number;
+        display.innerHTML+=number;
+    }
+    if(num1.length>10||num2.length>10){
+        display.innerHTML="Maxima cantidad de digitos ingresados!";
+    }
+}//fin funcion
+
+// funcion oppClick(codigoNumerico)
+
+function oppClick(numericCode){
+    operator=numericCode;
+    var oppString="";
+    flag=1;
+        if(operator === 4){
+            display.innerHTML+="/";
+            oppString="/";
+
+        }else if(operator === 3){
+            display.innerHTML+="*";
+            oppString="*";
+
+        }else if(operator === 2){
+            display.innerHTML+="-";
+            oppString="-";
+
+        }else{
+            display.innerHTML+="+";
+            oppString="+";
+
+        }//fin del else
+
+        if(flag === 1){
+            display.innerHTML=num1+oppString;
+        }
+
+        if(flag === 1 && num1 === ""){
+            clearButton();
+        } //final de if
+
+        if(equalTo === 1){
+            clearButton();
+        }//fin de if
+
+}//fin funcion
+
+//funcion equalClick
+function equalClick(){
+    equalTo = 1;
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    var result = "";
+    var roundedResult = "";
+
+    switch(true){
+        case(operator === 1):
+        result = num1+num2;
+        break;
+        case(operator === 2):
+        result = num1-num2;
+        break;
+        case(operator === 3):
+        result = num1*num2;
+        break;
+        case(operator === 4):
+        result = num1/num2;
+        break;
+    } //fin de switch
+    roundedResult = result.toFixed(4);
+    display.innerHTML = roundedResult;
+
+    if(roundedResult === "NaN"){
+        display.innerHTML="Operacion no valida!"
+    }
+}//fin funcion
